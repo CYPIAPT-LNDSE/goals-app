b62808a588724928eddf1673062489891d31caef
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-02-02 14:01:03 +0000

    add transition for progress review page, #44

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ function showhide(showpage, hidepage){
   $("."+hidepage).fadeOut(300, function(){
     $("."+showpage).show().attr('style','display:flex');
   });
+}
 
 function selectNewIcon (elem) {
   var selectedIcon = $('#' + elem);
