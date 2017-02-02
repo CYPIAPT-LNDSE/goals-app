5f3687014c104ead36390da6b9ac2b01e81dd5c6
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-02-02 11:47:18 +0000

    refactor code to one transition function, #44

--- public/js/index.js
+++ public/js/index.js
@@ -4,3 +4,10 @@ function showAddGoalPage(){
     $(".add-goal-page").show().attr('style', 'display: flex');
   });
 }
+
+function showhide(showpage, hidepage){
+  console.log("show:",showpage);
+  $("."+hidepage).fadeOut(300, function(){
+    $("."+showpage).show().attr('style','display:flex');
+  }).attr('style','display:none');
+}
