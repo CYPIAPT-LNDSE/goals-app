43074f879635bb0cb674dd36d58af421ccc1a8bf
emily <limeyb7@gmail.com>
2017-03-07 14:27:16 +0000

    renders hello world, sets up hot reloading

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var config = {
+   entry: './app/main.js',
+
+   output: {
+     filename: 'bundle.js',
+     path: __dirname
+   },
+
+   devServer: {
+      inline: true,
+      port: 8080
+   },
+
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
