a273f9d03d2eccea4f96a4e543648a9042ba4c47
emily <limeyb7@gmail.com>
2017-03-07 16:35:52 +0000

    basic redux setup, related #100

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,1 +1,14 @@
-const redux;
+/* reducers */
+// import reducers here
+
+const defaultState = {
+  // app state here
+};
+
+export default (state = defaultState, action) => {
+  switch(action.type) {
+    // cases here
+    default:
+      return state;
+  }
+};
