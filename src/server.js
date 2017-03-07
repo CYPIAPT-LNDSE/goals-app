12de9133b00cf42de3ff6695ef6d0d7777482de7
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-07 19:12:21 +0200

    Sets up hapi server with views to serve current static files Relates #81

---
+++ src/server.js
@@ -0,0 +1,50 @@
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Vision = require('vision');
+const Handlebars = require('handlebars');
+const fs = require('fs');
+
+const server = new Hapi.Server();
+
+// server.connection({
+//   port: process.env.PORT || 4000
+// });
+
+var tls = {
+    key : fs.readFileSync('./key.pem'),
+    cert : fs.readFileSync('./cert.pem')
+};
+
+server.connection({address: '0.0.0.0', port: process.env.PORT || 4000});
+
+server.register([Inert, Vision], (err) => {
+  if(err) throw err;
+
+  server.route({
+    path: '/{param*}',
+    method: 'GET',
+    handler: {
+      directory: {
+        path: 'public'
+      }
+    }
+  });
+});
+
+server.views({
+  engines: {
+    html: Handlebars
+  },
+  path: 'public/html',
+
+});
+
+server.route({
+  path: '/',
+  method: 'GET',
+  handler: (request, reply) => {
+    reply.view('landing-page');
+  }
+});
+
+module.exports = server;
