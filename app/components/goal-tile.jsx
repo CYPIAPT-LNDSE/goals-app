d5052f5cc7b9a25c2e4b2bb8002d0885c615f1cf
emily <limeyb7@gmail.com>
2017-03-08 16:38:09 +0000

    styling on goal tile without background pic or progress bar, related #96

--- app/components/goal-tile.jsx
+++ app/components/goal-tile.jsx
@@ -3,11 +3,23 @@ import React from 'react';
 const tile = props => {
 
   const goal = props.goal;
+  const pathAvatar = `./app/public/images/avatars/${goal.avatar}.png`;
 
-  return <div>
-    <p>{ goal.name }</p>
-  </div>
-
-}
+  return (
+    <div className="goalTile"> {/* outer div for goal tile */}
+      <div className="goalTile_progress"> {/* green background showing progress */}
+      </div>
+      <div className="goalTile_avatarContainer">
+        <img className="goalTile_img" src={ pathAvatar } />
+      </div>
+      <div className="goalTile_nameContainer">
+        <p>{ goal.name }</p>
+        <div className="goalTile_rating">
+          <p>7</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default tile;
