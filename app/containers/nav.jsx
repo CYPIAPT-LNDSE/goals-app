672b0219c3bbac27c46571f4390e61eaeff19095
emily <limeyb7@gmail.com>
2017-03-08 12:35:50 +0000

    adds and styles container element for back icon, relaed #87

--- app/containers/nav.jsx
+++ app/containers/nav.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
 const Nav = props => {
+
+  const path = "./app/public/icons/";
+  const pathBack = path + "back.svg";
+  const pathMenu = path + "menu.svg";
+
   return (
     <nav className="nav">
+      <div className="nav_backContainer">
+        <img className="back" src={ pathBack } />
+      </div>
       <div className="nav_logoContainer">
         <h1>
           Grow
         </h1>
       </div>
       <div className="nav_menuContainer">
-        <img className="menu" src="./app/public/icons/menu.svg" />
+        <img className="menu" src={ pathMenu } />
       </div>
     </nav>
   );
