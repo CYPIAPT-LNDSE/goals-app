c5fc9b3899c410aeb2a34d926b91924c10681eff
emily <limeyb7@gmail.com>
2017-03-09 16:27:45 +0000

    adds text input field to add-goal page with upper character limit of 50, related #89

--- app/components/add-new-goal/add-new-goal.jsx
+++ app/components/add-new-goal/add-new-goal.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 const addGoal = () => {
   return (
-    <p>Hello new goal</p>
+    <div className="addNewGoal">
+      <div className="newGoal_inputContainer-outer">
+        <label
+          htmlFor="newGoalInput"
+          className="newGoal_label"
+        >Pick a plant to grow alongside your goal</label>
+        <div className="newGoal_inputContainer-inner">
+          <textarea
+            name="new-goal"
+            id="newGoalInput"
+            maxLength="50"
+            className="newGoal_input"
+            type="text"
+            autoFocus
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
