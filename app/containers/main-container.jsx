d412245ddc7de11fcc926690ad9a1837c2723d3b
emily <limeyb7@gmail.com>
2017-03-09 16:01:53 +0000

    adapts main-container to load contend dependent on app state, related #89

--- app/containers/main-container.jsx
+++ app/containers/main-container.jsx
@@ -3,18 +3,30 @@ import { connect } from 'react-redux'
 
 /* components */
 import GoalsList from './../components/goals-list/goals-list.jsx';
+import AddGoal from './../components/add-new-goal/add-new-goal.jsx';
 
 const MainContent = props => {
+
+  let component;
+
+  switch (props.step.currentStep) {
+    case 'ADD_GOAL':
+      component = <AddGoal />
+      break;
+    default:
+      component = <GoalsList goals={ props.goals } />
+  }
+
   return (
-    <div
-      className="MainContent"
-    >
-      {/* show different views here depending on flow */}
-      <GoalsList goals = { props.goals } />
+    <div className="MainContent">
+      { component }
     </div>
   );
 }
 
-const mapStateToProps = state => ({ goals: state.goals });
+const mapStateToProps = state => ({
+  goals: state.goals,
+  step: state.step,
+});
 
 export default connect(mapStateToProps)(MainContent);
