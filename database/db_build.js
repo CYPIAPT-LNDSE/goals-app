b64943aa9de304f1eb90a7982d12956b86a20f36
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-09 19:53:30 +0200

    Adds js file to build database Relates #82

---
+++ database/db_build.js
@@ -0,0 +1,13 @@
+const env = require('env2');
+const fs = require('fs');
+
+env('../config.env');
+
+const dbConnection = require('./db_connection');
+
+const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+
+dbConnection.query(sql, (err, result) => {
+  if(err) throw(err);
+  console.log('Built database with result: ', result);
+});
