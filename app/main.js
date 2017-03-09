a254821060d501e38bb9a3eac1101d8fac62b268
emily <limeyb7@gmail.com>
2017-03-09 12:26:44 +0000

    moves everything inside provider component, related #108

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 /* components */
 import App from './App.jsx';
@@ -7,8 +8,12 @@ import App from './App.jsx';
 /* redux */
 import Store from './reducers/index.js';
 import { createStore } from 'redux';
+
 const store = createStore(Store);
 
-ReactDOM.render(<App store={store} />, document.getElementById('app'));
-
-store.subscribe(render);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
