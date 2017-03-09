9a73acb77cd54b38b771f5bd30b30d3a6eae2d02
emily <limeyb7@gmail.com>
2017-03-09 17:13:20 +0000

    creates new component for avatars on add-goal page, related #89

---
+++ app/components/add-new-goal/new-goal-avatars.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const Avatars = () => {
+
+  const pathToAvatars = `./app/public/images/avatars/`;
+
+  // could refactor to get avatars from state in case we want to load from DB?
+  const availableAvatars = [
+    'sprout',
+    'pepper',
+    'pumpkin',
+    'flower',
+  ];
+
+  const topRow = availableAvatars.slice(0, 2).map(av => {
+    return (
+      <div
+        className="newGoal_avatarBox"
+        key={`avatar_${av}`}
+        id={`avatar_${av}`}
+      >
+        <img src={`${pathToAvatars}${av}.png`} />
+      </div>
+    )
+  })
+
+
+
+  return (
+    <div className="newGoal_avatarsContainer">
+      <div className="newGoal_avatarsContainer-row">
+        { topRow }
+      </div>
+    </div>
+  )
+};
+
+export default Avatars;
