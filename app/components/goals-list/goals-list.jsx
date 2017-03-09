181c4e5006890fa204704e7a85f0db35cc74303e
emily <limeyb7@gmail.com>
2017-03-09 13:53:25 +0000

    removes inline styles from goals list, related #108

--- app/components/goals-list/goals-list.jsx
+++ app/components/goals-list/goals-list.jsx
@@ -8,17 +8,8 @@ const goalsList = () => {
     return <li key={ goal.id }>{ goal.name }</li>
   });
 
-  const style = {
-    background: 'url(app/public/images/goals-list/background.png)',
-    backgroundPosition: 'bottom center',
-    backgroundRepeat: 'no-repeat',
-  };
-
   return (
-    <div
-      className="page goalsList"
-      style={ style }
-      >
+    <div className="page goalsList">
       <div className="goalsList_buttonContainer">
         <div className="button-outer">
           <button type="button" name="button" className="goalsList_button">ADD A GOAL &nbsp;+</button>
