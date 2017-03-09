4855959bba5c89c83bb0c9a04cf85d5704cf01ca
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-03-09 14:03:10 +0000

    fixes main container code
    related #100

--- app/containers/main.jsx
+++ app/containers/main.jsx
@@ -4,17 +4,26 @@ import { connect } from 'react-redux'
 /* components */
 import GoalsList from './../components/goals-list/goals-list.jsx';
 
-let main = () => {
+// use caps for component names (even if pure)
+// Pls find a better name than main
+let Main = goals => { // props passed from map state to props come in here
   return (
     <div
       className="main"
     >
       {/* show different views here depending on flow */}
-      <GoalsList />
+      <GoalsList goals = { goals } {/*Need to data on to components*/}/>
     </div>
   );
 }
 
-main = connect()(main);
+
+// Each container should have a mapStateToProps
+// This get the part of the state relevent to the container.
+// It may also transform the state into a form that is more manageable for the
+//   view to consume
+const mapStateToProps = state => state.goals;
+
+main = connect(mapStateToProps)(main);
 
 export default main;
