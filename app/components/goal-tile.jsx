8fa52ab11782e7a99de66e4e8af20f2b3e921459
emily <limeyb7@gmail.com>
2017-03-10 10:05:45 +0000

    remove styling from jsx file, related #111

--- app/components/goal-tile.jsx
+++ app/components/goal-tile.jsx
@@ -5,11 +5,6 @@ const tile = props => {
   const goal = props.goal;
   const pathAvatar = `./app/public/images/avatars/${goal.avatar}.png`;
 
-  const style = {
-    // background: 'url(./app/public/images/list_box.png)',
-    // backgroundSize: '100% 100%',
-  }
-
   return (
     <div className="goalTile" style={ style }> {/* outer div for goal tile */}
       <div className="goalTile_progress"> {/* green background showing progress */}
