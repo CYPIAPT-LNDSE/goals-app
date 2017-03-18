d445a1a7633b1691ef2de6d3c3b91761209de6fd
emily <limeyb7@gmail.com>
2017-03-18 21:04:48 +0200

    adds new tests for reducer

--- tests/reducer-test.js
+++ tests/reducer-test.js
@@ -15,31 +15,40 @@ const defaultState = {
   currentGoal: null,
 };
 
-tape('test redux reducer steps', (t) => {
+tape('test reducer step_add_goal: step and previousStep changed', (t) => {
 
   const initialState = defaultState;
-
   const actionStepAddGoal = {
     type: types.STEP_ADD_GOAL,
   };
 
   t.equal(reducer(initialState, actionStepAddGoal).step, steps.ADD_GOAL, "step add goal sets correct step");
   t.equal(reducer(initialState, actionStepAddGoal).previousStep, steps.GOALS_LIST, "step add goal sets correct step");
-
   t.end();
 });
 
-tape('test redux input_goal', (t) => {
+tape('test reducer case input_goal: input value is added to state', (t) => {
 
   const initialState = { ...defaultState, step: steps.ADD_GOAL, };
-
   const input = 'I will test test t'
-
   const actionInputGoal = {
     type: types.INPUT_GOAL,
     input: input,
   };
 
   t.equal(reducer(initialState, actionInputGoal).newGoal.name, input, 'input value added to state');
+  t.end();
+});
+
+tape('test reducer case SELECT_AVATAR: newgoal.avatar value is updated', (t) => {
+
+  const initialState = { ...defaultState, step: steps.ADD_GOAL, };
+  const avatar = 'monkey';
+  const actionSelectAvatar = {
+    action: types.SELECT_AVATAR,
+    avatar: avatar,
+  };
+
+  t.equal(reducer(initialState, actionSelectAvatar).newGoal.avatar, avatar, 'avatar value updated');
   t.end();
 })
