adae792bace5126b9216efd4d9da4ef6542759d2
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-19 14:48:59 +0200

    Animate progress bar on view goal page Relates #94

--- app/components/view-goal/progress-bar.jsx
+++ app/components/view-goal/progress-bar.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const ProgressBarComponent = ({ rating, time }) => {
-  const style = {width: `${(rating*10)}%`};
-  const left = {left: `${(rating*10)-6}%`};
+  const style = {
+    width: `${(rating*10)}%`,
+    animation: `stretchRight${rating} 2s ease-out`};
+  const left = {
+    left: `${(rating*10)-6}%`,
+    animation: `slideRight${rating} 2s ease-out`};
   return (
     <div className="progress-bar-container">
       <div className="progress-bar-background">
