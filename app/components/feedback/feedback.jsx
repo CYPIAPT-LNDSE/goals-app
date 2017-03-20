f858bff302701aa4d13ed10e778b80e9e408b065
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-20 12:11:35 +0200

    Imports feedback.scss into style.scss Relates #90

--- app/components/feedback/feedback.jsx
+++ app/components/feedback/feedback.jsx
@@ -3,7 +3,7 @@ import GoalTileComponent from '../goal-tile.jsx';
 const Feedback = ({ currentGoal }) => {
   return (
     <div className='feedback-page'>
-      <div className="feedback-goal-tile-container">
+      <div className='feedback-goal-tile-container'>
         <GoalTileComponent goal={ currentGoal } />
       </div>
     </div>
