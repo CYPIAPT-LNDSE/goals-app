36dbcac0a8c99f7b21308f87b6f7e4a2ae38f339
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-20 12:33:42 +0200

    Adds question to request feedback Relates #90

--- app/components/feedback/feedback.jsx
+++ app/components/feedback/feedback.jsx
@@ -6,6 +6,9 @@ const Feedback = ({ currentGoal }) => {
       <div className='feedback-goal-tile-container'>
         <GoalTileComponent goal={ currentGoal } />
       </div>
+      <div className='feedback-question-container'>
+        <p>Would you like to explain why?</p>
+      </div>
     </div>
   );
 };
