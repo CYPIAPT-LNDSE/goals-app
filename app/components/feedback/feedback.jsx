562778c6d0bfa7a6ef4811be46829cfe53054ef9
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-03-20 12:06:13 +0200

    Add goal tile component Relates #90

--- app/components/feedback/feedback.jsx
+++ app/components/feedback/feedback.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import GoalTileComponent from '../goal-tile.jsx';
 const Feedback = ({ currentGoal }) => {
   return (
-    <div>
-      Hello World!
+    <div className='feedback-page'>
+      <div className="feedback-goal-tile-container">
+        <GoalTileComponent goal={ currentGoal } />
+      </div>
     </div>
   );
 };
