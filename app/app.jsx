1f919595f6fd57cc5d68ece3a248fd0a94c39d52
emily <limeyb7@gmail.com>
2017-03-21 16:22:34 +0200

    installs moment.js and formates times nicely on progress bars related #169 fixes buggy progress bar sliders

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux'
 import './scss/style.scss';
 import 'normalize.css';
+import moment from 'moment';
 
 /* import components */
 import MainContainer from './containers/main-container.jsx';
--- app/components/view-goal/progress-bar.jsx
+++ app/components/view-goal/progress-bar.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import moment from 'moment';
 
 const ProgressBarComponent = ({ rating, }) => {
-  const score = parseInt(rating.score);
+  const score = parseInt(rating.score) || 0;
   const style = {
     width: `${(score * 10)}%`,
-    animation: `stretchRight${score} 2s ease-out`};
+    animation: `stretchRight${score} 2s ease-out`
+  };
   const left = {
-    left: `${(score*10)-6}%`,
+    left: `${(score * 10) - (score > 0 ? 6 : 0)}%`,
     animation: `slideRight${score} 2s ease-out`};
   return (
     <div className="progress-bar-container">
       <div className="progress-bar-background" />
       <div className="progress-bar-foreground" style={style} />
       <div className="progress-bar-rating" style={left}>
-        <p>{ score }</p>
+        <p>{ (score || 0) }</p>
       </div>
       <div className="progress-bar-time">
         <p>
-          { rating.time.toString() }
+          { moment().format("ddd, hA") }
         </p>
       </div>
     </div>
