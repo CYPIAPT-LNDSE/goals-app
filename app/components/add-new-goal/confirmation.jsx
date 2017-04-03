de127ccf8da7a60d2e1e5b185d65f458475d3cd9
emily <limeyb7@gmail.com>
2017-04-03 17:41:54 +0300

    adds ok button to confirmation modal, related #143

--- app/components/add-new-goal/confirmation.jsx
+++ app/components/add-new-goal/confirmation.jsx
@@ -6,7 +6,7 @@ class ConfirmationModal extends React.Component {
   }
 
   componentDidMount() {
-    this.timer = setTimeout(this.props.saveGoal, 2000);
+    this.timer = setTimeout(this.props.saveGoal, 2500);
   }
 
   render() {
@@ -18,6 +18,11 @@ class ConfirmationModal extends React.Component {
           <path className="circ path" d="M30.5,6.5L30.5,6.5c6.6,6.6,6.6,17.4,0,24l0,0c-6.6,6.6-17.4,6.6-24,0l0,0c-6.6-6.6-6.6-17.4,0-24l0,0C13.1-0.2,23.9-0.2,30.5,6.5z"/>
            <polyline className="tick path" points="11.6,20 15.9,24.2 26.4,13.8 "/>
         </svg>
+        <a onClick={ ()=> {
+          clearTimeout(this.timer);
+          this.props.saveGoal();
+        }
+        }>Ok</a>
       </div>
     );
   }
