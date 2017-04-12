878ac6941b909ab3b550ee215d2247463c780190
emily <limeyb7@gmail.com>
2017-04-12 18:56:44 +0100

    fixes linting errors, related #222

--- server/database/db_build.js
+++ server/database/db_build.js
@@ -1,9 +1,6 @@
-const env = require('env2');
 const fs = require('fs');
 
-env('./config.env');
-
-const { client } = require('./db_connection');
+const { client, } = require('./db_connection');
 
 const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
@@ -11,11 +8,11 @@ client.connect((err) => {
   if (err) throw err;
   client.query(sql, (err, result) => {
     if (err) throw err;
-    
+
     // disconnect the client
     client.end((err) => {
       if (err) throw err;
-      console.log("YAAAs tables were created with result: ", result);
+      console.log('YAAAs tables were created with result: ', result);
     });
   });
 });
--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -2,23 +2,21 @@ const pg = require('pg');
 const Pool = pg.Pool;
 const url = require('url');
 
-require('env2')('./config.env');
-
 pg.defaults.ssl = true;
 
 if(!process.env.GROW_DB_URL) throw new Error('Environment variable GROW_DB_URL\
  must be set');
 
-var client = new pg.Client(process.env.GROW_DB_URL);
+const client = new pg.Client(process.env.GROW_DB_URL);
 
 const params = url.parse(process.env.GROW_DB_URL);
-const [username, password] = params.auth.split(':');
+const [ username, password, ] = params.auth.split(':');
 
 const options = {
   host: params.hostname,
   port: params.port,
   database: params.pathname.split('/')[1],
-  max: process.env.DB_MAX_CONNECTIONS || 2
+  max: process.env.DB_MAX_CONNECTIONS || 2,
 };
 
 options.username = username;
