1332056f05eb52db50cf130246bbd4b92b1f7c3e
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-04-12 12:56:05 +0300

    Assigns ssl to true for each connection Relates #218

--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -1,7 +1,10 @@
-const { Pool } = require('pg');
+const pg = require('pg');
+const Pool = pg.Pool;
 const url = require('url');
 
 require('env2')('./config.env');
+
+pg.defaults.ssl = true;
 
 if(!process.env.GROW_DB_URL) throw new Error('Environment variable GROW_DB_URL\
  must be set');
