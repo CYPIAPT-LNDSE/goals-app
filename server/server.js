e42a9cdf233d6ce378ebddcf373de13fcffcbbe8
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-04-13 12:20:45 +0300

    Refactor socket manager edited by merge Relates #154

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const hapi = require('hapi');
 const inert = require('inert');
 const fs = require('fs');
-const auth = require('hapi-auth-cookie');
+const cookieAuth = require('hapi-auth-cookie');
+const bell = require('bell');
+const { createSocket } = require('./sockets.js');
 
 require('env2')('./config.env');
 
 const routes = require('./index.js');
-const socket = require('./sockets.js');
 const server = new hapi.Server();
 
 server.connection({
@@ -18,20 +19,38 @@ server.connection({
   },
 });
 
-server.register([ inert, auth, ], (err) => {
+const cookieOptions = {
+  password: process.env.COOKIE_PASSWORD,
+  cookie: 'grow-cookie',
+  isSecure: process.env.NODE_ENV === 'PRODUCTION',
+  ttl: 24 * 60 * 60 * 1000,
+};
+
+const fbOptions = {
+  provider: 'facebook',
+  password: 'cookie_encryption_password_secure',
+  clientId: process.env.APP_FACEBOOK_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  isSecure: process.env.NODE_ENV === 'PRODUCTION',
+};
+
+server.register([ bell, ], (err) => {
   if (err) { throw new Error (err); }
 
-  server.auth.strategy('session', 'cookie', true, {
-    password: process.env.COOKIE_PASSWORD,
-    cookie: 'grow-user',
-    isSecure: process.env.NODE_ENV === 'PRODUCTION',
-    ttl: 30 * 24 * 60 * 60 * 1000,
-    redirectTo: '/login',
-  });
-
-  server.route(routes);
+  server.auth.strategy('facebook', 'bell', fbOptions);
 });
 
-socket(server.listener);
+server.register([ inert, cookieAuth,], (err) => {
+  if (err) { throw new Error (err); }
 
-module.exports = server;
+  server.auth.strategy('session', 'cookie', 'required', cookieOptions);
+  server.route(routes);
+
+});
+
+createSocket(server.listener);
+// const getUserData = require('./getUserData')(io);
+
+module.exports = {
+  server: server,
+};
