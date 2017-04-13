5d6e0511f79d5fa5fb99b377b66d56830b0956a3
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-04-13 13:04:47 +0300

    Fixes comments for database connection pr Fixes #222

--- server/database/db_connection.js
+++ server/database/db_connection.js
@@ -2,6 +2,8 @@ const pg = require('pg');
 const Pool = pg.Pool;
 const url = require('url');
 
+require('env2')('./config.env');
+
 pg.defaults.ssl = true;
 
 if(!process.env.GROW_DB_URL) throw new Error('Environment variable GROW_DB_URL\
@@ -9,21 +11,4 @@ if(!process.env.GROW_DB_URL) throw new Error('Environment variable GROW_DB_URL\
 
 const client = new pg.Client(process.env.GROW_DB_URL);
 
-const params = url.parse(process.env.GROW_DB_URL);
-const [ username, password, ] = params.auth.split(':');
-
-const options = {
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  max: process.env.DB_MAX_CONNECTIONS || 2,
-};
-
-options.username = username;
-options.password = password;
-options.ssl = (options.host !== 'localhost');
-
-module.exports = {
-  client: client,
-  pool: new Pool(options),
-};
+module.exports = client;
