383294f9010da4dc044c79c9e1f677d97c5b358c
emily <limeyb7@gmail.com>
2017-04-16 18:55:48 +0100

    refactors with new working database connection related #225 and #218

--- server/database/get-user.js
+++ server/database/get-user.js
@@ -1,43 +1,31 @@
 const dbClient = require('./db_connection.js');
 
 const findUser = `
-  SELECT * from users
-  WHERE username = $1;`;
+SELECT * from users
+WHERE username = $1;`;
 
 const createUser = `
-  INSERT into users (username)
-  VALUES ($1)
-  RETURNING user_id;`;
+INSERT into users (username)
+VALUES ($1)
+RETURNING user_id;`;
 
 module.exports = (facebookId, callback) => {
 
-  dbClient.connect((err) => {
-    if (err) throw new Error (err);
+  dbClient.query(findUser, [ facebookId, ], (getErr, result) => {
+    if (getErr) {
+      return callback('database error while retrieving user details');
+    }
 
-    dbClient.query(findUser, [ facebookId, ], (getErr, result) => {
-      if (getErr) {
-        return callback('database error while retrieving user details');
+    if (result.rows.length) {
+      return callback(null, result.rows[0].user_id);
+    }
+
+    dbClient.query(createUser, [ facebookId, ], (insertErr, newUser) => {
+      if (insertErr) {
+        return callback('unable to create new user');
       }
 
-      if (result.rows.length) {
-        dbClient.end((err) => {
-          if (err) throw err;
-
-          callback(null, result.rows[0].user_id);
-        });
-      } else {
-        dbClient.query(createUser, [ facebookId, ], (insertErr, newUser) => {
-          if (insertErr) {
-            callback('unable to create new user');
-          }
-
-          dbClient.end((err) => {
-            if (err) throw err;
-
-            callback(null, newUser);
-          });
-        });
-      }
+      callback(null, newUser);
     });
   });
 };
