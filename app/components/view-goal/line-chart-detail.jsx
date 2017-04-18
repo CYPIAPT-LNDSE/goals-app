f12096cb4e6acd480095276490a5701dc28bd914
emily <limeyb7@gmail.com>
2017-04-18 16:41:16 +0100

    adds basic layout to component with styling related #241

--- app/components/view-goal/line-chart-detail.jsx
+++ app/components/view-goal/line-chart-detail.jsx
@@ -8,7 +8,15 @@ const LineChartDetail = ({ currentGoal, }) => {
      <div className="goal-detail-goal-tile-container">
        <GoalTileComponent goal={ currentGoal } />
      </div>
-     Hello
+     <div className="line-chart-container-detail">
+       <div className="line-chart-detail-feedback-container">
+         <p className="detail-feedback">
+           January 1st 2017
+           This is my rating
+         </p>
+       </div>
+       <div className="detail-chart-container"></div>
+     </div>
    </div>
   );
 };
