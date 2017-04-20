7e47166a7cfaac4bb9f220469006c76430b9df9f
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-04-20 18:05:14 +0300

    Queries the database with a join to get goals with ratings Relates #189

--- server/database/get-user-data.js
+++ server/database/get-user-data.js
@@ -1,22 +1,22 @@
 const dbClient = require('./db_connection.js');
 
-const getGoals = `
-SELECT title, rating, icon, date_created
-FROM goals
-WHERE user_id = $1 AND deleted = false;`;
+const getGoals = `SELECT goals.user_id, goals.goal_id, goals.title,
+goals.icon, ratings.rating, ratings.comment, ratings.date_created
+FROM (goals LEFT OUTER JOIN ratings ON goals.goal_id = ratings.goal_id)
+WHERE goals.user_id=$1 AND goals.deleted=false
+GROUP BY goals.goal_id, ratings.rating_id`;
+
 
 const getUserData = (id) => {
-  //get user goals
 
   dbClient.query(getGoals, [ id, ], (goalsErr, goalsRes) => {
     if(goalsErr) {
       console.log(goalsErr);
       return '';
     }
-    const goals = goalsRes.rows;
     console.log(goalsRes.rows);
   });
-  // for each goal get the ratings
+
   return id;
 };
 
