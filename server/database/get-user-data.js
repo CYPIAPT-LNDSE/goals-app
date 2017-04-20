8309c26592be5c352814b3c82ed5096ad5756f99
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-04-20 20:05:30 +0300

    Formats the user data received from database Relates #189

--- server/database/get-user-data.js
+++ server/database/get-user-data.js
@@ -1,23 +1,22 @@
 const dbClient = require('./db_connection.js');
+const formatUserData = require('./format-user-data');
 
-const getGoals = `SELECT goals.user_id, goals.goal_id, goals.title,
-goals.icon, ratings.rating, ratings.comment, ratings.date_created
+const getGoals = `SELECT goals.user_id, goals.goal_id, goals.title, goals.icon,
+ratings.rating_id, ratings.rating, ratings.comment, ratings.date_created
 FROM (goals LEFT OUTER JOIN ratings ON goals.goal_id = ratings.goal_id)
 WHERE goals.user_id=$1 AND goals.deleted=false
-GROUP BY goals.goal_id, ratings.rating_id`;
+GROUP BY goals.goal_id, ratings.rating_id
+ORDER BY goals.goal_id, ratings.rating ASC`;
 
 
 const getUserData = (id) => {
-
   dbClient.query(getGoals, [ id, ], (goalsErr, goalsRes) => {
     if(goalsErr) {
       console.log(goalsErr);
       return '';
     }
-    console.log(goalsRes.rows);
+    return formatUserData(goalsRes.rows);
   });
-
-  return id;
 };
 
 module.exports = getUserData;
