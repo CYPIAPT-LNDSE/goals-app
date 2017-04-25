1b7732700425e6656dffbc43a8afa1c5e4fc4b57
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-04-25 12:20:12 +0300

    Refactors get goals query into 2 separate queries Relates #189

--- server/helpers/format-user-data.js
+++ server/helpers/format-user-data.js
@@ -1,22 +1,4 @@
-const includesId = (goals, id) => {
-  let flag = false;
-  goals.forEach((goal) => {
-    if (goal.id === id) flag = true;
-  });
-  return flag;
-};
-
-const removeDuplicates = (goals) => {
-  let filteredGoals = [];
-  goals.forEach((goal) => {
-    if(!includesId(filteredGoals, goal.id)) {
-      filteredGoals.push(goal);
-    }
-  });
-  return filteredGoals;
-};
-
-const formatGoalsArray = (goals) => {
+module.exports = (goals) => {
   return goals.map((goal) => {
     return {
       id: goal.goal_id,
@@ -26,27 +8,4 @@ const formatGoalsArray = (goals) => {
       ratings: [],
     };
   });
-};
-
-const addRatings = (formattedGoals, goalsArray) => {
-  goalsArray.forEach( (goal) => {
-    formattedGoals.forEach ((formattedGoal, index) => {
-      if(goal.goal_id === formattedGoals[index].id){
-        formattedGoals[index].ratings =
-        goal.rating ? formattedGoals[index].ratings.concat([{
-          id: goal.rating_id,
-          score: goal.rating,
-          comment: goal.comment,
-          time: goal.date_created,
-        },]) : [];
-      }
-    });
-  });
-  return formattedGoals;
-};
-
-module.exports = (goalsArray) => {
-  let goals = removeDuplicates(formatGoalsArray(goalsArray));
-  goals = addRatings(goals, goalsArray);
-  return JSON.stringify(goals);
 };
