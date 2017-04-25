a71c59841ad5cb62bc7be7575df3267e6448cab0
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-04-25 12:37:28 +0300

    Adds general action to main container Relates #189

--- app/actions/general.js
+++ app/actions/general.js
@@ -1,6 +1,6 @@
 import * as types from './../action_types.js';
 
-export const receiveData = data => {
+export const onReceiveData = data => {
   return {
     type: types.RECEIVE_DB_DATA,
     goals: JSON.parse(data),
