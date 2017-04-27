e1846026dde58fc6a586208fc7cf00978e1844dc
emily <limeyb7@gmail.com>
2017-04-27 15:04:43 +0100

    latest rating selected by default when going to line-chart-detail view related #286

--- app/components/view-goal/line-chart-detail.jsx
+++ app/components/view-goal/line-chart-detail.jsx
@@ -4,65 +4,78 @@ import moment from 'moment';
 import GoalTileComponent from '../goal-tile.jsx';
 import LineChart from './line-chart.jsx';
 
-const LineChartDetail = ({ currentGoal, onSelectRating, }) => {
+class LineChartDetail extends React.Component {
 
-  const allRatings = currentGoal.ratings.slice(0).reverse();
+  componentDidMount() {
+    const currentGoal = this.props.currentGoal;
+    const latestRating = currentGoal.ratings[0];
+    if (latestRating) {
+      this.props.onSelectRating(latestRating.id);
+    }
+  }
 
-  const containerStyle = {
-    width: allRatings.length < 6
-      ? '100%'
-      : 400 + (allRatings.length - 3) * 10,
-  };
+  render() {
 
-  const feedbackStyle = {
-    opacity: currentGoal.ratingSelected
-      ? 1
-      : 0,
-  };
+    const currentGoal = this.props.currentGoal;
 
-  const time = currentGoal.ratingSelected
-    ? currentGoal.ratingSelected.time
-    : {};
+    const allRatings = currentGoal.ratings.slice(0).reverse();
 
-  const score = currentGoal.ratingSelected
-    ? currentGoal.ratingSelected.score
-    : null;
+    const containerStyle = {
+      width: allRatings.length < 6
+          ? '100%'
+          : 400 + (allRatings.length - 3) * 10,
+    };
 
-  const comment = currentGoal.ratingSelected
-    ? currentGoal.ratingSelected.comment
-    : '';
+    const feedbackStyle = {
+      opacity: currentGoal.ratingSelected
+          ? 1
+          : 0,
+    };
 
-  return (
-   <div className="line-chart-detail goal-detail-page">
-     <div className="goal-detail-goal-tile-container">
-       <GoalTileComponent goal={ currentGoal } />
-     </div>
-     <div className="line-chart-container-detail">
-       <div className="line-chart-detail-feedback-container" style={ feedbackStyle }>
-         <p className="detail-feedback-time">
-           { moment(time).format('LLLL') }
-         </p>
-         <p className="detail-feedback-score">
-           You rated your progress { score }/10
-         </p>
-         <p className="detail-feedback-comment">
-           { comment }
-         </p>
+    const time = currentGoal.ratingSelected
+        ? currentGoal.ratingSelected.time
+        : {};
+
+    const score = currentGoal.ratingSelected
+        ? currentGoal.ratingSelected.score
+        : null;
+
+    const comment = currentGoal.ratingSelected
+        ? currentGoal.ratingSelected.comment
+        : '';
+
+    return (
+     <div className="line-chart-detail goal-detail-page">
+       <div className="goal-detail-goal-tile-container">
+         <GoalTileComponent goal={ currentGoal } />
        </div>
-       <div className="detail-chart-container">
-         <div className="detail-chart-container-inner" style={ containerStyle }>
-           <LineChart
-             avatar={ currentGoal.avatar }
-             ratings={ allRatings }
-             isChartPreview={ false }
-             onSelectRating={ onSelectRating }
-           />
+       <div className="line-chart-container-detail">
+         <div className="line-chart-detail-feedback-container" style={ feedbackStyle }>
+           <p className="detail-feedback-time">
+             { moment(time).format('LLLL') }
+           </p>
+           <p className="detail-feedback-score">
+             You rated your progress { score }/10
+           </p>
+           <p className="detail-feedback-comment">
+             { comment }
+           </p>
+         </div>
+         <div className="detail-chart-container">
+           <div className="detail-chart-container-inner" style={ containerStyle }>
+             <LineChart
+               avatar={ currentGoal.avatar }
+               ratings={ allRatings }
+               isChartPreview={ false }
+               onSelectRating={ this.props.onSelectRating }
+             />
+           </div>
          </div>
        </div>
      </div>
-   </div>
-  );
-};
+    );
+  }
+}
 
 LineChartDetail.propTypes = {
   currentGoal: React.PropTypes.object,
