a97e8056d17b6983796fece001cf07e0eb818322
Shireen Zoaby <shireen.zoaby@gmail.com>
2017-04-29 11:51:04 +0300

    Changes class names in add new goal page Relates #214

--- app/components/add-new-goal/add-new-goal.jsx
+++ app/components/add-new-goal/add-new-goal.jsx
@@ -14,10 +14,10 @@ const addGoal = ({ newGoal, onInputGoal, onSelectAvatar, saveNewGoal, triggerCon
     : null;
 
   return (
-    <div className="addNewGoal">
+    <div className="add-new-goal">
       { modal }
-      <div className="newGoal_inputContainer-outer">
-        <label htmlFor="newGoalInput" className="newGoal_label">
+      <div className="new-goal-input-container-outer">
+        <label htmlFor="newGoalInput" className="new-goal-label">
           Set a goal and choose a plant to grow with it
         </label>
       <NewGoalInput newGoal={ newGoal } onInputGoal={ onInputGoal } />
@@ -26,12 +26,12 @@ const addGoal = ({ newGoal, onInputGoal, onSelectAvatar, saveNewGoal, triggerCon
         onSelectAvatar={ onSelectAvatar }
         newGoal={ newGoal }
       />
-      <div className="newGoal_buttonContainer">
+    <div className="new-goal-button-container">
         <div className="button-outer">
           <button
             type="button"
             name="button"
-            className="newGoal_button"
+            className="new-goal-button"
             disabled={ !newGoal.name }
             onClick={ triggerConfirmation }
           >ADD</button>
