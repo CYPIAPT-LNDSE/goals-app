25689d4646e4fd009542acbb2666705e1393f53a
emily <limeyb7@gmail.com>
2017-05-01 10:56:29 +0100

    fixes loading data spinner, related #309

--- app/components/goals-list/goals-list.jsx
+++ app/components/goals-list/goals-list.jsx
@@ -1,43 +1,52 @@
 import React from 'react';
 
+/* components */
 import GoalTile from '../goal-tile.jsx';
 import DeleteModal from './delete-modal.jsx';
+import LoadingModal from './../loading-modal.jsx';
 
 import * as steps from './../../steps.js';
 
-const GoalsList = ({ goals, stepAddGoal, onSelectGoal,
-  onBorderClick, onDeleteGoal, onEditGoal, deleteModal, toggleDeleteModal, }) => {
+const createGoalListItem = (goal, props) => (
+  <li key={ goal.id } >
+    <GoalTile
+      goal={ goal }
+      step={ steps.GOALS_LIST }
+      onSelectGoal={ props.onSelectGoal }
+      visibleEditDelete={ goal.visibleEditDelete }
+      onBorderClick={ props.onBorderClick }
+      onDeleteGoal={ props.onDeleteGoal }
+      onEditGoal={ props.onEditGoal }
+      toggleDeleteModal={ props.toggleDeleteModal }
+    />
+  </li>
+);
 
-  const goalsListItems = goals.map(goal => {
-    return (
-      <li key={ goal.id } >
-        <GoalTile
-          goal={ goal }
-          step={ steps.GOALS_LIST }
-          onSelectGoal={ onSelectGoal }
-          visibleEditDelete={ goal.visibleEditDelete }
-          onBorderClick={ onBorderClick }
-          onDeleteGoal={ onDeleteGoal }
-          onEditGoal={ onEditGoal }
-          toggleDeleteModal={ toggleDeleteModal }
-        />
-      </li>);
-  });
+const GoalsList = props => {
+
+  const goals = props.goals;
+
+  const goalsListItems = goals.map(goal => createGoalListItem(goal, props));
 
   const dynamicStyle = {
     height: Math.max(window.innerHeight - 90, goals.length * 108 + 115),
   };
 
-  const overlay = deleteModal.display
+  const loading = !props.dataLoaded
+    ? <LoadingModal />
+    : null;
+
+  const overlay = props.deleteModal.display
     ? <DeleteModal
-        toggleDeleteModal={ toggleDeleteModal }
-        deleteModal={ deleteModal }
-        onDeleteGoal={ onDeleteGoal }
+        toggleDeleteModal={ props.toggleDeleteModal }
+        deleteModal={ props.deleteModal }
+        onDeleteGoal={ props.onDeleteGoal }
       />
     : null;
 
   return (
     <div className="page goalsList" style={ dynamicStyle }>
+      { loading }
       { overlay }
       <div className="goalsList_buttonContainer">
         <div className="button-outer">
@@ -45,7 +54,7 @@ const GoalsList = ({ goals, stepAddGoal, onSelectGoal,
             type="button"
             name="button"
             className="goalsList_button"
-            onClick={ stepAddGoal }
+            onClick={ props.stepAddGoal }
           >ADD A GOAL &nbsp;+</button>
         </div>
       </div>
@@ -56,16 +65,22 @@ const GoalsList = ({ goals, stepAddGoal, onSelectGoal,
   );
 };
 
-GoalsList.propTypes = {
-  goals: React.PropTypes.array,
-  stepAddGoal: React.PropTypes.func,
+createGoalListItem.propTypes = {
   onSelectGoal: React.PropTypes.func,
   visibleEditDelete: React.PropTypes.bool,
   onBorderClick: React.PropTypes.func,
   onDeleteGoal: React.PropTypes.func,
   onEditGoal: React.PropTypes.func,
   toggleDeleteModal: React.PropTypes.func,
+};
+
+GoalsList.propTypes = {
+  goals: React.PropTypes.array,
+  stepAddGoal: React.PropTypes.func,
+  toggleDeleteModal: React.PropTypes.func,
+  onDeleteGoal: React.PropTypes.func,
   deleteModal: React.PropTypes.object,
+  dataLoaded: React.PropTypes.boolean,
 };
 
 export default GoalsList;
