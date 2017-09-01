bc89750bdb2ed3c8431b546060d6a7f5cd83717e
emily <limeyb7@gmail.com>
2017-09-01 13:41:43 +0100

    adds extra log statements to DB model files related #319

--- server/model/get-user.js
+++ server/model/get-user.js
@@ -3,9 +3,9 @@ const dbClient = require('./../database/db_connection.js');
 const queries = require('./../database/queries.js');
 
 module.exports = (userId, callback) => {
-
   dbClient.query(queries.getUserById, [ userId, ], (getErr, result) => {
     if (getErr) {
+      console.log(getErr); // eslint-disable-line no-console
       return callback('database error while retrieving user details');
     }
 
@@ -15,6 +15,7 @@ module.exports = (userId, callback) => {
 
     dbClient.query(queries.insertUser, [ userId, ], (insertErr, newUser) => {
       if (insertErr) {
+        console.log(insertErr); // eslint-disable-line no-console
         return callback('unable to create new user');
       }
 
